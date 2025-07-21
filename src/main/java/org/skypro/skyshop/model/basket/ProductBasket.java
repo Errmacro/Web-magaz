e509f922eaedf2f554007442baebcce426eff73a
Alexandra <saa-saa@mail.ru>
2025-07-21 22:05:44 +0300

    Веб-магаз ДЗ

---
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -0,0 +1,25 @@
+package org.skypro.skyshop.model.basket;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class ProductBasket {
+    private final Map<UUID, Integer> totalBasket;
+
+    public ProductBasket() {
+        totalBasket = new HashMap<>();
+    }
+
+    public void addProduct(UUID key, Integer value) {
+        totalBasket.put(key, value);
+    }
+
+    public Map<UUID, Integer> getTotalBasket() {
+        return totalBasket.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+
+}
